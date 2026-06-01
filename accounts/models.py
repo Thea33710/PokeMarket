3885dc3b503e1e8e9f1298657f2b319f8a51c1ca
Thea33710 <thea33710@gmail.com>
2026-06-01 15:14:28 +0200

    feat: ajouter modèle User personnalisé + migration

---
+++ accounts/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """Modèle utilisateur personnalisé pour PokéMarket."""
+
+    pseudo = models.CharField(max_length=80, unique=True)
+    pokemon_avatar_id = models.IntegerField(null=True, blank=True)
+    code_ami_switch = models.CharField(max_length=15, null=True, blank=True)
+
+    VISIBILITE_CHOICES = [
+        ('public', 'Public'),
+        ('prive', 'Privé'),
+        ('non_renseigne', 'Non renseigné'),
+    ]
+    visibilite_code_ami = models.CharField(
+        max_length=13,
+        choices=VISIBILITE_CHOICES,
+        default='prive'
+    )
+
+    nb_echanges = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.pseudo
--- pokemarket/settings.py
+++ pokemarket/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'accounts.User'
