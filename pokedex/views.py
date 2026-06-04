cb0b512ddbd650a83ee96449579dd3f05f5a386f
Thea33710 <thea33710@gmail.com>
2026-06-04 11:11:54 +0200

    fix: supprimer import Jeu inutilisé dans views.py

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Jeu, Pokedex, PokemonCache
+from .models import Pokedex, PokemonCache
 from .forms import PokedexForm
 
 
