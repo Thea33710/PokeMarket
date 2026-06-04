1a44688fac8e439ccce64875af8ad63fb86603d0
Thea33710 <thea33710@gmail.com>
2026-06-04 14:43:46 +0200

    fix: supprimer import HttpResponse inutilisé

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
-from django.http import HttpResponse
 from .models import Pokedex, PokemonCache
 from .forms import PokedexForm
 
